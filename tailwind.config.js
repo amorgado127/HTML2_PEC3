447bb33d5874420846e84b4d027129f61e2c1034
amorgadol <amorgado127@gmail.com>
2020-12-30 15:06:24 +0100

    Commit 2 PEC3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,45 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
-  purge: ["./src/**/*.html"],
+  purge:[ './src/**/*.html',],
+    
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        teal: colors.teal
+        gray: { personal: "#f2f2f2", estatico: "#d3dae4" },
+        blue: {
+          principal: "#007cba",
+          formulario: "#cbeef2",
+          nogrid: "#0b0b1a",
+        },
       },
-      container: {
-        center: true,
-        padding: "1rem"
-      }
-    }
+      maxHeight: {
+        "200px": "200px",
+      },
+      maxWidth: {
+        "30%": "30%",
+        "60%": "60%",
+        "90%": "90%",
+        "95%": "95%",
+      },
+      width: {
+        "288px": "288px",
+        "320px": "320px",
+        "90%": "90%",
+        "95%": "95%",
+      },
+      height: {
+        "90%": "90%",
+        "95%": "95%",
+        "192px": "192px",
+        "260px": "260px",
+        "240px": "240px",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
-  variants: {
-    extend: {}
-  },
-  plugins: []
+  variants: {},
+  plugins: [],
 };
